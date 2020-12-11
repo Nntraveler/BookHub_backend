a2bed04e66fb5699c2be473e813f90cdb913452a
Never <897013703@qq.com>
2020-12-11 20:45:27 +0800

    Consultation model编写

---
+++ src/main/java/com/bookhub/model/Consultation.java
@@ -0,0 +1,13 @@
+package com.bookhub.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Consultation {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+}
